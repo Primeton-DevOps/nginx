091746ae3a27cf694005c077d566154e06c44b48
ZhongWen Li <lizhongwen1989@gmail.com>
2016-10-20 15:08:37 +0800

    Update

--- web/example/html/js/index.js
+++ web/example/html/js/index.js
@@ -2,13 +2,39 @@ function localResource() {
     window.location = "test.html";
 }
 
-function remoteService() {
+function remoteService(method) {
+    if (method == "POST") {
+        add();
+    } else if (method == "GET") {
+        query();
+    } else {
+        query();
+    }
+
+}
+
+function add() {
     var json = {"id": "1001", "name": "Nginx"};
     $.ajax({
         url : "/api/rest/users",
         type : 'POST',
         data : json,
         cache : false,
+        contentType : 'application/json',
+        success : function(text) {
+            alert(text);
+        },
+        error: function(text) {
+            alert(text);
+        }
+    });
+}
+
+
+function query() {
+    $.ajax({
+        url : "/api/rest/users",
+        type : 'GET',
         contentType : 'application/json',
         success : function(text) {
             alert(text);
